48ba9612fa6e75010154b3c9dec33fee0fb93077
Paul Kern <pkern@laas.fr>
2022-03-30 09:45:26 +0200

    Completed structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fi:
+    long_description = fi.read()
+
+setuptools.setup(
+	name="gentsp",
+	version="0.0.1",
+	author="Paul 'charon25' Kern",
+	description="Travelling Salesman Problem solver using a genetic algorithm",
+	long_description=long_description,
+    long_description_content_type='text/markdown',
+	python_requires=">=3.7",
+	url="https://www.github.com/charon25/GenticTSPSolver",
+	license="MIT",
+	packages=['gentsp']
+)
